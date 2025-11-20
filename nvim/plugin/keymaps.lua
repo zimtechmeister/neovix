d4999b448948deb667818d2010882adbaff40533
Tim Zechmeister <tim.zechmeister03@gmail.com>
2025-11-20 19:18:47 +0100

    improved keybind to exit terminal mode

--- nvim/plugin/keymaps.lua
+++ nvim/plugin/keymaps.lua
@@ -12,7 +12,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C-Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- Exit nvim saving the filecontent and a session file to the current working directory
 -- Load the session with nvim -S Session.vim or :source Session.vim
